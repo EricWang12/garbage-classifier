fabaec4a0547a1c610a0cc58992bd5e134ddfa6c
Yuheng Chen <ccyyhh97@hotmail.com>
2019-10-20 11:19:51 -0500

    upd

--- screens/StatsScreen.js
+++ screens/StatsScreen.js
@@ -8,43 +8,68 @@ const hazardousColor = '#FA876E';
 const trashColor = '#ffffff';
 const colors = [recycleableColor, compostColor, hazardousColor, trashColor];
 
-export default class LinksScreen extends React.Component{
-  constructor(props){
+export default class LinksScreen extends React.Component {
+  state = {
+    counts: []
+  }
+
+  constructor(props) {
     super(props);
 
     let param = props.navigation.state.params;
+    this.setState({
+      counts: param
+    });
     console.log(param);
   }
-  render(){
+  render() {
+    let counts = this.state.counts;
 
     const data = [
-      { x: "Recyclable", y: 5 },
-      { x: "Compost", y: 12 },
-      { x: "Hazardous", y: 2 },
-      { x: "Trash", y: 23 }
+      { x: "recyclable", y: 5 },
+      { x: "compost", y: 12 },
+      { x: "hazardous", y: 2 },
+      { x: "trash", y: 23 }
     ];
-    
-  return (
 
-    <ScrollView style={styles.scroll}>
-      <Text style={styles.display}>Summary</Text>
-      <View style={styles.container}>
-        <VictoryPie
-          colorScale={colors}
-          data={data}
-          height={300}
-          style={{ labels: { fontSize: 18, padding: 15, fill: "white" } }}
-        />
-      </View>
+    data = counts.map(e => {
+      for (let i = 0; i < data.length; i++) {
+        let category = data[i];
+        if (e.name == category.x) {
+          return { x: category.x, y: e.Count }
+        }
+      }
+      // data.forEach(category=>{
+      //   if (e.name == category.x) {
+      //     return { x: e.x, y: data.Count }
+      //   }
+      // })
 
-      {/* <Button onPress={handlePress}></Button> */}
+    })
 
-    </ScrollView>
-  );
+    console.log(data);
+
+    return (
+
+      <ScrollView style={styles.scroll}>
+        <Text style={styles.display}>Summary</Text>
+        <View style={styles.container}>
+          <VictoryPie
+            colorScale={colors}
+            data={data}
+            height={300}
+            style={{ labels: { fontSize: 18, padding: 15, fill: "white" } }}
+          />
+        </View>
+
+        {/* <Button onPress={handlePress}></Button> */}
+
+      </ScrollView>
+    );
   }
 }
 
-const handlePress = (e)=>{
+const handlePress = (e) => {
   console.log(this.props);
 }
 
